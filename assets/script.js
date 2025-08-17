d3c767f58d507e6413eb0f90f09125e217655d5d
Arya Ramachandran <arytheauthor@gmail.com>
2025-08-17 18:23:11 +0530

    new story and edits

--- assets/script.js
+++ assets/script.js
@@ -29,6 +29,7 @@
   // Blog posts (edit/add here)
   const posts = [
     { title: "Last Without Anything", date: "26-04-2023", url: "./posts/first-post.html", tag: "Short Story", excerpt: "How did she disappear?."},
+    { title: "Our Autistic Kid", date: "29-02-2024", url: "./posts/second-post.html", tag: "Short Story", excerpt: "How did he behave?."}
   ];
 
   function postCard(p){ return `
