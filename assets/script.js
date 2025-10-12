48118af6c85640b4627bc5018cdaa2932a44dfda
Arya <aryatheauthor@gmail.com>
2025-10-12 11:31:11 +0530

    new story changes

--- assets/script.js
+++ assets/script.js
@@ -30,7 +30,8 @@
   const posts = [
     { title: "Last Without Anything", date: "2023-04-26", url: "./posts/first-post.html", tag: "Short Story", excerpt: "How did she disappear?."},
     { title: "Our Autistic Kid", date: "2024-02-29", url: "./posts/second-post.html", tag: "Short Story", excerpt: "How did he behave?."},
-    { title: "Shenanigans Ending the year", date: "2024-06-15", url: "./posts/third-post.html", tag: "Short Story", excerpt: "How did the year it end?."}
+    { title: "Shenanigans Ending the year", date: "2024-06-15", url: "./posts/third-post.html", tag: "Short Story", excerpt: "How did the year it end?."},
+    { title: "Simpli", date: "2024-06-15", url: "./posts/four-post.html", tag: "Short Story", excerpt: "Simp?."}
   ];
 
 
